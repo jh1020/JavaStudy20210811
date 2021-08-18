406f0c5383bb3f252f2fed0599ef3a5493bab956
jiho <wwjh3002@naver.com>
2021-08-18 21:09:27 +0900

    QUIZ

--- 원지호3002/src/baekJun/Main.java
+++ 원지호3002/src/baekJun/Main.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		//시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 
-		//89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D,
-		//나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+		//예를 들어, 좌표가 (12, 5)인 점 A는 x좌표와 y좌표가 모두 양수이므로 제1사분면에 속한다. 점 B는 x좌표가 음수이고 y좌표가 양수이므로 제2사분면에 속한다.
+//
+	//	점의 좌표를 입력받아 그 점이 어느 사분면에 속하는지 알아내는 프로그램을 작성하시오. 단, x좌표와 y좌표는 모두 양수나 음수라고 가정한다.
 
 		Scanner input = new Scanner(System.in);
-		int Score = input.nextInt();
-		if (Score >= 90) {
-			System.out.println("A");
-		}else if (Score >= 80){
-			System.out.println("B");
-		}else if (Score >= 70){
-			System.out.println("C");
-		}else if (Score >= 60){
-			System.out.println("D");
-		}else 
-			System.out.println("F");
+		int number = input.nextInt();
+		int number2 = input.nextInt();
+		
+		if (number > 0 && number2 > 0) {
+			System.out.println("1");
+		}else if (number > 0 && number2 < 0) {
+			System.out.println("4");
+		}else if (number < 0 && number2 > 0) {
+			System.out.println("2");
+		}else
+			System.out.println("3");
 		
 			
 		
