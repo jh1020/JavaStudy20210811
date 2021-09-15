feaa3e223b2bf7ae6ea630f31bf4bc16a91f7fc5
jiho <wwjh3002@naver.com>
2021-09-15 21:55:21 +0900

    2021-09-15

---
+++ 원지호3002/src/b18_컬렉션/Collection.java
@@ -0,0 +1,28 @@
+package b18_컬렉션;
+
+import java.util.Collection;
+import java.util.List;
+
+/*
+ * 
+ *  컬렉션(Colection)
+ *  java에서의 데이터 관리를 위한 라이브러리
+ *  
+ *  list: 순서 O
+ *  set: 순서 X 추
+ *  map:  key : value
+ *  
+ *  추가 속도: set < list
+ *  삭제 속도: list < set
+ *  
+ *  
+ *  
+ */
+
+
+public class Collection {
+	Collection c ;
+	List<String> li;
+		
+	
+}
